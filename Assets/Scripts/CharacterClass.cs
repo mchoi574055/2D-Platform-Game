f1d4526acd97962540b4db35b002968f87a42794
TimmyTwoD <david12345l@gmail.com>
2021-07-25 00:52:18 -0700

    Left, right, jump movement and border hitboxes

--- Assets/Scripts/CharacterClass.cs
+++ Assets/Scripts/CharacterClass.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class CharacterClass : MonoBehaviour
 {
     Vector2 _initialPos;
-
+    Vector2 jumpPos;
+    Vector2 jumpLeftPos;
+    Vector2 jumpRightPos;
+    private Rigidbody2D rb;
+    //[SerializeField] private float _launchpower = 150;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().isKinematic = true;
+        GetComponent<Rigidbody2D>().isKinematic = false;
         _initialPos = new Vector2(0, 0);
+        jumpRightPos = new Vector2(200, 400);
+        jumpLeftPos = new Vector2(-200, 400);
+        jumpPos = new Vector2(0, 400);
         transform.position = _initialPos;
     }
 
@@ -19,16 +26,36 @@ public class CharacterClass : MonoBehaviour
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log ("update test");
+        } 
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
 
-    }
 
     private void OnMouseDrag()
     {
-        Vector3 movePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = movePos;
+        //Vector3 movePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = movePos;
+        if (Input.GetKeyDown(KeyCode.A)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+              GetComponent<Rigidbody2D>().AddForce(jumpPos);
+        } 
     }
 }
