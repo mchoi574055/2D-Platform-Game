55a034569f5c2538b21ae032a9dbbfd830bd6119
Michael Choi <mchoi574055@g.ucla.edu>
2021-07-25 21:12:28 -0700

    Sprite axis movements now move appropriately at a good and constant velocity.

--- Assets/Scripts/CharacterClass.cs
+++ Assets/Scripts/CharacterClass.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CharacterClass : MonoBehaviour
 {
-    /*
+    
     Vector2 _initialPos;
     Rigidbody2D _rb;
+
+    // DEFINE VELOCITY VALUES
+    Vector2 rightVelocity;
+    Vector2 leftVelocity;
+    Vector2 downVelocity;
+    Vector2 upVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.isKinematic = false;
         _initialPos = new Vector2(0, 0);
         transform.position = _initialPos;
+
+        // INITIALIZE VELOCITY VALUES
+        rightVelocity = new Vector2(5, 0);
+        leftVelocity = new Vector2(-5, 0);
+        downVelocity = new Vector2(0, -5);
+        upVelocity = new Vector2(0, 5);
     }
 
     // Update is called once per frame
@@ -37,6 +48,12 @@ public class CharacterClass : MonoBehaviour
             Debug.Log("A key was pressed");
             moveLeft();
         }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed");
+            moveUp();
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -52,19 +69,28 @@ public class CharacterClass : MonoBehaviour
 
     private void moveRight()
     {
-        _rb.velocity = new Vector3(10, 0, 0);
+        if (_rb.velocity.x < rightVelocity.x)
+            _rb.velocity = rightVelocity;
+
     }
 
 
     private void moveDown()
     {
-        
+        if (_rb.velocity.y > downVelocity.y)
+            _rb.velocity = downVelocity;
     }
 
 
     private void moveLeft()
     {
-
+        if (_rb.velocity.x > leftVelocity.x)
+            _rb.velocity = leftVelocity;
     }
-    */
+    
+    private void moveUp()
+    {
+        if (_rb.velocity.y < upVelocity.y)
+            _rb.velocity = upVelocity;
+    }
 }
